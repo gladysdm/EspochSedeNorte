f1c5e1377bc566430a317e639285fce85b3d6c27
GladisDM <gladysmarr123@gmail.com>
2024-11-25 15:23:17 -0500

    Mi primer commit

---
+++ src/main/java/ec/espoch/edu/ec/espochsedenorte/EspochSedeNorte.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.espoch.edu.ec.espochsedenorte;
+
+/**
+ *
+ * @author USER
+ */
+public class EspochSedeNorte {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Hola");
+        System.out.println("Hola Hola");
+    }
+}
